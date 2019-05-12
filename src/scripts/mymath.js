fec2e5c3b0fe1468b84df7b864ae4d1f6536c567
NJul <nina.dev.web@gmail.com>
2019-05-12 12:16:56 +0300

    ECMAScript modules

--- src/scripts/mymath.js
+++ src/scripts/mymath.js
@@ -14,17 +14,33 @@ function divide(a, b) {
   return a / b;
 }
 
-class Graph {
+export default class Graph {
   addNode() {
     console.log('node added');
   }
 }
 const PI = 3.1415;
 
-export {
+/* export {
   add,
   subtract,
   multiply,
   divide,
   PI
+}; */
+
+export {
+  add as a,
+  subtract,
+  multiply as mt,
+  divide as d,
+  PI
 };
+
+/* Экспортируем по умолчанию класс Graph */
+// export default Graph;
+/* Только один дефолтный экспорт разрешен для одного модуля. */
+
+/* 2 способа есть: Именованный экспорт, тогда вам надо в фигурных скобках просто перечислить названия всего того, что вы собираетесь экспортировать */
+/* Либо export default единственный экспорт, который не имеет своего собственного имени. Он может идти под любым именем. */
+/* Переименовывать можно с обоих сторон, либо со стороны экспорта, либо со стороны импорта. subtract as s */
